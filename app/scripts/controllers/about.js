e1c008bd1495d78d025e62f56c4443a4419c2721
Rinaldy <rinaldy97@gmail.com>
2017-10-16 18:26:15 +0100

    Fixed picture bug

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -14,10 +14,17 @@
 
   AboutCtrl.$inject = [];
   function AboutCtrl() {
+
+    //<editor-fold desc="Variables">
     var vm = this;
+    var avatarRinaldy = '../images/ninja.png';
+    var avatarSivarjuen = '../images/avatar.png';
+    var avatarKaranvir = '../images/avatar.png';
+    //</editor-fold>
 
     //<editor-fold desc="Functions">
     vm.openLink = openLink;
+    vm.getAvatar = getAvatar;
     //</editor-fold>
 
     vm.members = [
@@ -25,27 +32,36 @@
         name: 'Karanvir Singh Kalsi',
         university: 'University of Birmingham',
         course: 'MSci Computer Science w/ Industrial Year',
-        avatar: '../images/avatar.png',
+        avatar: 'karanvir',
         link: 'https://github.com/kalsi97'
       },
       {
         name: 'Sivarjuen Ravichandran',
         university: 'University of Birmingham',
         course: 'MSci Computer Science w/ Industrial Year',
-        avatar: '../images/avatar.png',
+        avatar: 'sivarjuen',
         link: 'https://github.com/Sivarjuen'
       },
       {
         name: 'Rinaldy Ridwan',
         university: 'University of Birmingham',
         course: 'BSc Computer Science',
-        avatar: '../images/avatar.png',
+        avatar: 'rinaldy',
         link: 'https://github.com/Rinaldyr'
       }
     ];
 
     function openLink(link) {
       window.open(link);
+    }
+
+    function getAvatar(whos) {
+      switch (whos) {
+        case 'rinaldy': return avatarRinaldy;
+        case 'sivarjuen': return avatarSivarjuen;
+        case 'karanvir': return avatarKaranvir;
+        default: return '../images/avatar.png';
+      }
     }
   }
 })();
